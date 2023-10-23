83d9e4b120449df272591ffcf312483bc75f12b1
Roger Coll <rogercoll@protonmail.com>
2023-10-23 12:00:44 +0200

    feat: add python script

---
+++ say_hello.py
@@ -0,0 +1,10 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python replace_slashes.py <path>")
+    sys.exit(1)
+
+argument = sys.argv[1]
+argument = argument.replace("\\", "/")
+
+print(argument)
